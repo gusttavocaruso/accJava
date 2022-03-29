d5af9d2299aa0b2adde781319fdaa3d7c9d078cd
gustacaru <gustavoalmeida26@hotmail.com>
2022-03-29 09:35:14 -0300

    first commit on Eclipse

---
+++ HelloJavaWorld/src/trybe/main/HelloJavaWorld.java
@@ -0,0 +1,10 @@
+package trybe.main;
+
+public class HelloJavaWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello big Java");
+	}
+
+}
