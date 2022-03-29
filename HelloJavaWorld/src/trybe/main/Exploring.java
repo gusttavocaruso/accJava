cdbe35994982dc588fef737810fc3afa0c53c141
gustacaru <gustavoalmeida26@hotmail.com>
2022-03-29 12:07:43 -0300

    add exploring file

---
+++ HelloJavaWorld/src/trybe/main/Exploring.java
@@ -0,0 +1,15 @@
+package trybe.main;
+
+public class Exploring {
+
+	public static void main(String[] args) {
+		byte x = 0;
+		int a = 10, b = 20;
+		
+		String phrase = "Estou tentando entender o Java e o Eclipse";
+		System.out.println(x);
+		System.out.println(a + b);
+		System.out.println(phrase);
+	}
+
+}
