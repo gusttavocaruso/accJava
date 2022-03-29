de29d1bb03f93433efdc7d10913fe32ad6d76cdc
gustacaru <gustavoalmeida26@hotmail.com>
2022-03-29 13:03:31 -0300

    update in HelloJW project

--- HelloJavaWorld/src/trybemain/HelloJavaWorld.java
+++ HelloJavaWorld/src/trybemain/HelloJavaWorld.java
@@ -3,7 +3,7 @@ package trybemain;
 public class HelloJavaWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello big Java");
+		System.out.println("Hello big-Java, take-easy ma friend");
 	}
 
 }
