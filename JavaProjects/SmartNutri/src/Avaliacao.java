de061b8e5989925534665102678452dfb396310d
gustacaru <gustavoalmeida26@hotmail.com>
2022-04-01 09:00:05 -0300

    add style update

--- JavaProjects/SmartNutri/src/Avaliacao.java
+++ JavaProjects/SmartNutri/src/Avaliacao.java
@@ -2,9 +2,9 @@ import java.lang.Math;
 
 public class Avaliacao {
 
-	public void calcularIMC(double peso, double altura) {
-		double IMC = peso / Math.pow(altura, 2);
-		System.out.println("O seu IMC é " + IMC);
-	}
+  public void calcularIMC(double peso, double altura) {
+    double IMC = peso / Math.pow(altura, 2);
+    System.out.println("O seu IMC é " + IMC);
+  }
 
 }
